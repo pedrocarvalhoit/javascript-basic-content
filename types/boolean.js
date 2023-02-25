9c19d91af8f181ae8e498a6994b5d9ccddfb8a35
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-02-25 18:25:45 +0000

    String

---
+++ types/boolean.js
@@ -0,0 +1,11 @@
+const numberOne = 5;
+const numberTwo = 10;
+
+let activeRegister = true;
+
+if(activeRegister === true){
+    console.log("Hello Pedro!");
+}
+
+console.log(numberOne === numberTwo);
+console.log(numberOne !== numberTwo);
