f29d457742b3049c00794f068395377d858d92d9
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-02-25 18:26:43 +0000

    ConsoleLog

---
+++ console/consoleLog.js
@@ -0,0 +1,12 @@
+const myVariable = "Hello World";
+console.log("My variable is: ", myVariable);
+
+const name = "João";
+console.log(`The name is ${name}.`);
+
+const person = {
+    name: "Maria",
+    age: 30,
+    job: "Dev",
+};
+console.log(person);
