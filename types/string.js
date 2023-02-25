b1fa1648866714bf7fb31194395b9d86dc3d6f24
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-02-25 18:28:00 +0000

    String

---
+++ types/string.js
@@ -0,0 +1,17 @@
+//Basic
+const text1 = "Hello World";
+const numberString = "3165123123";
+
+const quote = "Pedro says 'Hii!'"
+console.log(quote);
+
+//Unicode convert
+const cipher = '\u0024'
+const capitalA = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cipher)
+console.log(capitalA)
+console.log(tique)
+console.log(hiragana)
