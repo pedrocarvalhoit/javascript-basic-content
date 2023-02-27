f479316ffc178ed61ec8732b027f5cf94001dfbc
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-02-27 22:17:41 +0000

    Ternary

---
+++ jsParticularities/ternary.js
@@ -0,0 +1,11 @@
+const minAge = 18;
+const clientAge = 16;
+
+if(clientAge >= minAge){
+    console.log("Beer")
+}else{
+    console.log("Juice")
+}
+
+//ternary condicional             //true     //false
+console.log(clientAge >= minAge ? "beer" : "juice")
