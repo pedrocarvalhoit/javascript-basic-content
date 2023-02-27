2be3ab83d80d4071024dc34144e320edc547afab
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-02-27 22:17:07 +0000

    Function

---
+++ functions/function.js
@@ -0,0 +1,21 @@
+//Text
+function print(text){
+    console.log(text)
+}
+print("Hi i am jiujitsu blackbelt");
+
+//Sum
+function sum(num1, num2){
+    return(num1 + num2)
+}
+console.log(sum(22, 22));
+
+
+//Round
+function round(number){
+    return Math.round(number);
+}
+
+//One function inside other function
+print(sum(25, 25));
+print(round(2.654));
