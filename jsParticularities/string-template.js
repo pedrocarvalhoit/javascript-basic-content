869580d4d4b317753cc72cce312014930ee7c0b3
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-02-27 22:17:27 +0000

    String Template

---
+++ jsParticularities/string-template.js
@@ -0,0 +1,7 @@
+const name = "Pedro";
+const lastName = "Carvalho";
+const age = 2023 - 1992;
+
+const presentation = `My name is ${name}, i have ${age} years old and my last name is ${lastName} `;
+
+console.log(presentation);
