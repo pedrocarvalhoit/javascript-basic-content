48c5b5734e2f9fa89e95ae8f1dee5335c767a350
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-03-05 19:32:31 +0000

    Two Dimension List

--- array/twoDimensionList.js
+++ array/twoDimensionList.js
@@ -6,3 +6,17 @@ const studentsWithAvarages = [students, averages];
 //0 call students and 1 call Juliana
 console.log(`Position 1 student is: ${studentsWithAvarages[0][1]}, and the avarege is ${studentsWithAvarages[1][1]}`);
 
+//Student searching
+function searchForStudent(student){
+    if (studentsWithAvarages[0].includes(student)) {
+        const [students, avarages] = studentsWithAvarages;
+
+        console.log(`The student ${student} is in the list`);
+        const index = students.indexOf(student);
+        const avarege = avarages[index];
+        console.log(`${student} has avarage ${avarege}`)
+    }else{
+        console("Student not found")
+    }
+}
+searchForStudent("João");
