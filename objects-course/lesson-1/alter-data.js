20505a99c479251e42bfdf0fce0e8e8496a8e15e
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-03-20 09:38:52 +0000

    Alter object data

---
+++ objects-course/lesson-1/alter-data.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "Thamires",
+    phone: "999666555",
+};
+//Trying get email
+console.log(person.email);
+//Seting email
+person.email = "thamires.mkt@thamires.com"
+console.log(person.email);
+
+//Alter name
+person.name = "Thamires Holany";
+console.log(person);
