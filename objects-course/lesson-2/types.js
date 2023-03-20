27ec956d9b38a3dfa3bb17ee1a07edf83f5c1ff4
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-03-20 09:45:15 +0000

    Objects with more than 1 itens

---
+++ objects-course/lesson-2/types.js
@@ -0,0 +1,10 @@
+const client ={
+    name: "Pedro",
+    age: 30,
+    document: "11112222333",
+    email: "pedro@pedro.com",
+    phone: ["111111", "222222"],
+};
+console.log(client);
+console.log("---------");
+console.log(client.phone);
