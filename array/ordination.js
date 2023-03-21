c8396f3de681be3a14e3bbd88b63708281a92a17
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-03-21 18:48:24 +0000

    Ordinating a list

--- array/ordination.js
+++ array/ordination.js
@@ -1,11 +1,13 @@
+const books = require('./books-list')
+
 const prices = [25,15,30,50,45,10];
 
 let cheaper = 0;
 
-for (let actual = 0; actual < prices.length; actual++) {
-    if (prices[actual] < prices[cheaper]) {
+for (let actual = 0; actual < books.length; actual++) {
+    if (books[actual].price < books[cheaper].price) {
         cheaper = actual
     }
 }
 
-console.log(prices[cheaper]);
+console.log(books[cheaper].price);
