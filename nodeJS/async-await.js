af04ab4ccf5710fb44e12195b967561ec8fe7be5
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-04-13 12:09:06 +0100

    Async and Await

---
+++ nodeJS/async-await.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function errorHandler(error){
+    throw new Error(chalk.red(error.code, 'File does not exist'))
+}
+
+async function fileGeter(filePath){
+    try {
+        const encoding = 'utf-8';
+        const text = await fs.promises.readFile(filePath, encoding)
+        console.log(chalk.yellow(text));
+    } catch (error) {
+        errorHandler(error)
+    }
+
+}
+
+fileGeter('./files/text.md');   
+console.log("---------")
+fileGeter('./files/tet.md');   
+ 
+
