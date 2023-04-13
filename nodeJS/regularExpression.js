f9f4e8c35c8e6e8c527fa0f32f09b422927e10e0
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-04-13 19:33:24 +0100

    Regex

---
+++ nodeJS/regularExpression.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+const textTest = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).'
+
+function extractLinks(text){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captures = regex.exec(text);
+    console.log(captures);
+}
+
+extractLinks(textTest);
+
+
