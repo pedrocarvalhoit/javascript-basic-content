6da8591f5bd59ef53c5405ef0111512dfac93f8f
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-05-11 22:23:46 +0100

    Index

---
+++ opp-course/class-structure/index.js
@@ -0,0 +1,7 @@
+import User from "./User.js";
+import Admin from "./Admin.js";
+import Teacher from "./Teacher.js";
+
+const user1 = new User('Pedro Carvalho', 'ppp@gmail.com');
+console.log(user1.showInfos())
+
