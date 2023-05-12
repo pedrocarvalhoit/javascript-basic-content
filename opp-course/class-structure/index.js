6ddd38c3161688156a1080147cacdd22603b2f37
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-05-12 20:33:49 +0100

    index

--- opp-course/class-structure/index.js
+++ opp-course/class-structure/index.js
@@ -2,6 +2,8 @@ import User from "./User.js";
 import Admin from "./Admin.js";
 import Teacher from "./Teacher.js";
 
-const user1 = new User('Pedro Carvalho', 'ppp@gmail.com');
+const user1 = new User('Pedro ','Carvalho', 'ppp@gmail.com');
 console.log(user1.showInfos())
 
+const teacher1 = new Teacher('Andre ','Duarte', "andre@.com");
+console.log(teacher1.showInfos())
