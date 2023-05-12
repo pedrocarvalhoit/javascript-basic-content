4b49bc40526c8fcdfedb7a62b8628f1f93fa328a
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-05-12 20:33:05 +0100

    Admin

--- opp-course/class-structure/Admin.js
+++ opp-course/class-structure/Admin.js
@@ -5,6 +5,10 @@ export default class Admin extends User{
         super(name, email, birthday, role, active)
     }
 
+    showInfos(){
+        return `Admin: ${this.name}.`
+    }
+
     buildCourse(courseName, spotsAvailable){
         return `New ${courseName}, with ${spotsAvailable} spots opened`
     }
